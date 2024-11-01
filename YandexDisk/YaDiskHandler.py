de6ffb771f15b5c551a5212ac4ff1613be3db6c8
MRedor <mashapalchenkova@gmail.com>
2024-11-01 14:18:30 +0100

    renamed YaDisk root folder

--- YandexDisk/YaDiskHandler.py
+++ YandexDisk/YaDiskHandler.py
@@ -66,7 +66,7 @@ def __search_in_directory__(directory: str,
 def get_last_added_files(last_updated_time: datetime.datetime, ya_disk_info: YaDiskInfo):
     check_token(ya_disk)
     try:
-        __search_in_directory__('/DesignBot/', last_updated_time, ya_disk_info)
+        __search_in_directory__('/TelegramBot/', last_updated_time, ya_disk_info)
     except Exception as e:
         ya_disk_info.clear()
         raise Exception("Can't find any files")
@@ -99,7 +99,7 @@ def __add_nodes__(directory: str, last_updated_time, tree: Tree):
     for item in ya_disk.listdir(directory):
         if item.is_dir() and (not is_images(item)) and (not is_font(item)):
             if last_updated_time < item.created:
-                if directory == "/DesignBot/":
+                if directory == "/TelegramBot/":
                     tree.insert("root", item.name)
                 else:
                     tree.insert(directory[directory.rfind('/') + 1:], item.name)
@@ -110,7 +110,7 @@ def __add_nodes__(directory: str, last_updated_time, tree: Tree):
 def update_tree(tree: Tree, last_updated_time):
     check_token(ya_disk)
     __delete_nodes__('/', tree)
-    __add_nodes__('/DesignBot/', last_updated_time, tree)
+    __add_nodes__('/TelegramBot/', last_updated_time, tree)
     with open("./Tree/ObjectTree.pkl", "wb") as fp:
         pickle.dump(tree, fp)
 
