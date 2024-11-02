a88628e322195b8cc1bfebce72206df2a1df3d51
MRedor <mashapalchenkova@gmail.com>
2024-11-02 15:09:04 +0100

    new text for user-testing advanced slides

--- TelegramHandler/keyboards/start_and_simple_button.py
+++ TelegramHandler/keyboards/start_and_simple_button.py
@@ -1,4 +1,5 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import json
 
 
 def rows_for_main_menu():
@@ -108,7 +109,10 @@ async def choose_template_text_root(type_file: str) -> str:
         return f"У нас очень много шрифтов — тебе для какого подразделения нужны?\n"
     if type_file == 'search_by_tags':
         return f"Я подскажу варианты, как можно оформить твой контент!\n" \
-           f"Но для начала, подскажи, в каком шаблоне ты делаешь презентацию?\n"
+            f"К сожалению, прямо сейчас у я умею работать только с шаблоном Go, но если тебе очень нужен другой," \
+            f"пиши {json.load(open('./config.json'))['owner']} \n\n" \
+            f"Продолжаем?"
+           # f"Но для начала, подскажи, в каком шаблоне ты делаешь презентацию?\n\n" \
     if type_file == 'about_company':
         return f"У меня подготовлены слайды на двух языках — тебе какие нужны?"
 
